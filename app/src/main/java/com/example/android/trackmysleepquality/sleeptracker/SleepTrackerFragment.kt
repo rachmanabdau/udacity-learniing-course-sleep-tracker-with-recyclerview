834c10c061c918c0f02e388aea908b4234154f26
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2019-05-05 04:40:48 +0700

    Refresh Data with DiffUtil.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -102,7 +102,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(this, Observer {
             it.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
